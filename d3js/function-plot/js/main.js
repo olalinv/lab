856e6fff0873c6ef54c62349289af8172a54a963
Óscar Lalín <robottitto@gmail.com>
2017-07-04 11:47:25 +0200

    D3.js Function Plot

---
+++ d3js/function-plot/js/main.js
@@ -0,0 +1,19 @@
+functionPlot({
+  target: '#myFunction',
+  data: [{
+      fn: 'sin(x)',
+      color: 'red'
+    },
+    {
+      fn: 'cos(x)',
+      color: 'blue'
+    }
+  ],
+  grid: true,
+  yAxis: {
+    domain: [-1, 1]
+  },
+  xAxis: {
+    domain: [0, 2 * Math.PI]
+  }
+});
