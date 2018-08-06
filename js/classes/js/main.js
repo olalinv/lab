bc8e13510bbeb6fc19a58ec71bc18873dd08877d
Robottitto <robottitto@gmail.com>
2018-08-06 23:54:08 +0200

    JS Classes

--- js/classes/js/main.js
+++ js/classes/js/main.js
@@ -7,13 +7,36 @@ class Thing {
   }
   // Methods
   doSomething (thing) {
-    console.log(`This thing has an attribute '${Object.keys(thing)[0]}' with value '${thing.attr1}' and an attribute '${Object.keys(thing)[1]}' with value '${thing.attr2}'`);
+    console.log(
+      `This thing has: 
+      an attribute '${Object.keys(thing)[0]}' with value '${thing.attr1}' and 
+      an attribute '${Object.keys(thing)[1]}' with value '${thing.attr2}'`);
   }
 }
 
-// Thing 1
-let thing1 = new Thing('value1', 'value2');
-thing1.doSomething(thing1);
-// Thing 2
-let thing2 = new Thing('valueA', 'valueB');
-thing2.doSomething(thing2);
+// Inheritance
+class SubThing extends Thing {
+  constructor (attr1, attr2, attr3) {
+    // Attributes
+    super(attr1, attr2);
+    this.attr3 = attr3;
+    console.log(this);
+  }
+  // Methods
+  doSomething (thing) {
+    console.log(
+      `This subthing has: 
+      an attribute '${Object.keys(thing)[0]}' with value '${thing.attr1}', 
+      an attribute '${Object.keys(thing)[1]}' with value '${thing.attr2}' and 
+      an attribute '${Object.keys(thing)[2]}' with value '${thing.attr3}'`
+    );
+  }
+}
+
+// A thing
+let thing = new Thing('value1', 'value2');
+thing.doSomething(thing);
+
+// A subThing
+let subthing = new SubThing('value1', 'value2', 'value3');
+subthing.doSomething(subthing);
