6a8522e53430bbc4715f9bc38b68e6b6ff51cd08
Robottitto <robottitto@gmail.com>
2018-08-06 23:20:43 +0200

    JS Classes

--- js/classes/js/main.js
+++ js/classes/js/main.js
@@ -1,9 +1,19 @@
 class Thing {
   constructor (attr1, attr2) {
+    // Attributes
     this.attr1 = attr1;
     this.attr2 = attr2;
+    console.log(this);
+  }
+  // Methods
+  doSomething (thing) {
+    console.log(`This thing has an attribute 'attr1' with value '${thing.attr1}' and an attribute 'attr2' with value '${thing.attr2}'`);
   }
 }
 
-let thing = new Thing('Thing1', 'Thing2');
-console.log(thing);
+// A thing
+let thing1 = new Thing('Value1', 'Value2');
+thing1.doSomething(thing1);
+// Another thing
+let thing2 = new Thing('ValueA', 'ValueB');
+thing2.doSomething(thing2);
