b8f0361cde1cbd331112ecc1f47e947b8023f4ad
Robottitto <robottitto@gmail.com>
2018-08-23 14:00:37 +0200

    JS for...of

---
+++ js/for-of/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Array initialization
+let things = ['thing1', 'thing2', 'thing3', 'thing4', 'thing5'];
+
+// Iterate over array items
+for (let item of things) {
+  console.log(item);
+}
