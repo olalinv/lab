b12278882ef7257b6207752b47f7d03931562dbf
Robottitto <robottitto@gmail.com>
2018-08-24 13:58:18 +0200

    JS Classes as expressions

---
+++ js/classes-as-expressions/js/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Classes
+let Thing = class Thing {
+  constructor (attr1, attr2) {
+    // Attributes
+    this.attr1 = attr1;
+    this.attr2 = attr2;
+    console.log(this);
+  }
+  // Methods
+  saySomething () {
+    console.log(`I'm a thing`);
+  }
+  explainYourself (thing) {
+    console.log(
+      `I have: 
+      an attribute '${Object.keys(thing)[0]}' with value '${thing.attr1}', 
+      an attribute '${Object.keys(thing)[1]}' with value '${thing.attr2}'`);
+  }
+}
+
+// Subclasses
+let SubThing = class SubThing extends Thing {
+  constructor (attr1, attr2, attr3) {
+    // Attributes
+    super(attr1, attr2);
+    this.attr3 = attr3;
+    console.log(this);
+  }
+  // Methods
+  saySomething () {
+    super.saySomething();
+    console.log(`I'm a subthing`);
+  }
+  explainYourself (subthing) {
+    console.log(
+      `I have: 
+      an attribute '${Object.keys(subthing)[0]}' with value '${subthing.attr1}', 
+      an attribute '${Object.keys(subthing)[1]}' with value '${subthing.attr2}', 
+      an attribute '${Object.keys(subthing)[2]}' with value '${subthing.attr3}'`
+    );
+  }
+}
+
+// A thing
+let thing = new Thing('value1', 'value2');
+thing.saySomething();
+thing.explainYourself(thing);
+console.log(thing.constructor.name);
+
+// A subthing
+let subthing = new SubThing('value1', 'value2', 'value3');
+subthing.saySomething();
+subthing.explainYourself(subthing);
+console.log(subthing.constructor.name);
