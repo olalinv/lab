e2f9d1e9e28711c80963c0a41493be73139a952b
Robottitto <robottitto@gmail.com>
2018-08-27 13:51:45 +0200

    JS Custom error

---
+++ js/custom-error/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Create a new object, that prototypically inherits from the Error constructor
+function MyError (message) {
+  this.name = 'MyError';
+  this.message = message || 'Default Message';
+  this.stack = (new Error()).stack;
+}
+
+MyError.prototype = Object.create(Error.prototype);
+MyError.prototype.constructor = MyError;
+
+try {
+  throw new MyError();
+} catch (e) {
+  console.log(e.name); // 'MyError'
+  console.log(e.message); // 'Default Message'
+}
+
+try {
+  throw new MyError('custom message');
+} catch (e) {
+  console.log(e.name); // 'MyError'
+  console.log(e.message); // 'custom message'
+}
