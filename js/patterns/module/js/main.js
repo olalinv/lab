2dcc57aa1beebc11e19e68470aaa1733e96240b0
Robottitto <robottitto@gmail.com>
2018-08-28 14:19:36 +0200

    JS Patterns

---
+++ js/patterns/module/js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// myModule
+var myModule = (function () {
+  // Properties
+  var _privateProperty = 'Hello World';
+  var publicProperty = 'I am a public property';
+  // Methods
+  function _privateMethod () {
+    console.log(_privateProperty);
+  }
+  function publicMethod () {
+    _privateMethod();
+  }
+  // Public properties and methods
+  return {
+    publicMethod: publicMethod,
+    publicProperty: publicProperty
+  };
+}());
+
+myModule.publicMethod(); // outputs 'Hello World'
+console.log(myModule.publicProperty); // outputs 'I am a public property'
+console.log(myModule._privateProperty); // is undefined protected by the module closure
+myModule._privateMethod(); // is TypeError protected by the module closure
