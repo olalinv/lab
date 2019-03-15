66197027b51a53b1fca9d7fcb450eaec1457dc2b
robottitto <robottitto@gmail.com>
2019-03-15 03:19:11 +0100

    JS Web Components

--- js/web-components/js/main.js
+++ js/web-components/js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
-import './w/button.js';
+import './w/w-button.js';
 
-const button = document.querySelector('w-button');
+const wButton = document.querySelector('w-button');
+wButton.addEventListener('someEvent', (event) => {
+  console.log(event);
+  alert(`You´ve just launched a custom event called ${event.type}`);
+})
---
+++ js/web-components/js/w/w-button.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <link rel="stylesheet" href="./js/w/w-button.css">
+  <div class="w-button"></div>
+`;
+
+export class WButton extends HTMLElement {
+  static get observedAttributes() {
+    return ['title'];
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
+    this.buttonElement.innerHTML = this._title;
+  }
+
+  constructor() {
+    super();
+
+    // Template
+    this.root = this.attachShadow({ mode: 'open' });
+    this.root.appendChild(template.content.cloneNode(true));
+
+    // Elements
+    this.buttonElement = this.root.querySelector('.w-button');
+
+    // Events
+    this.root.addEventListener('click', () => {
+      this.dispatchEvent(new CustomEvent('someEvent', { 
+        detail: 'someValue'
+      }));
+    })
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    if (attrName === 'title' & oldValue !== newValue) {
+      this.title = newValue;
+    }
+  }
+
+}
+
+customElements.define('w-button', WButton);
