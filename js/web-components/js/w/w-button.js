16ff4b02ab4ce9adc28589942a9f6a3d66491969
robottitto <robottitto@gmail.com>
2019-03-15 03:27:18 +0100

    JS Web Components

--- js/web-components/js/w/w-button.js
+++ js/web-components/js/w/w-button.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// Template
 const template = document.createElement('template');
 template.innerHTML = `
   <link rel="stylesheet" href="./js/w/w-button.css">
   <div class="w-button"></div>
 `;
 
+// Class
 export class WButton extends HTMLElement {
+
   static get observedAttributes() {
     return ['title'];
   }
@@ -32,12 +35,13 @@ export class WButton extends HTMLElement {
 
     // Events
     this.root.addEventListener('click', () => {
-      this.dispatchEvent(new CustomEvent('someEvent', { 
+      this.dispatchEvent(new CustomEvent('someEvent', {
         detail: 'someValue'
       }));
     })
   }
 
+  // Attributes
   attributeChangedCallback(attrName, oldValue, newValue) {
     if (attrName === 'title' & oldValue !== newValue) {
       this.title = newValue;
